d1ccd09bbddda6e75ce1dee0d81a9466e89cbdbd
Svetlana Dyakova <dyakova030@gmail.com>
2026-06-21 17:32:59 +0300

    Настроить JWT-аутентификацию

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,3 +1,16 @@
+from django.urls import include, path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 urlpatterns = [
+    path(
+        "v1/jwt/create/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path("v1/jwt/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("v1/jwt/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
