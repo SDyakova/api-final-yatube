491d387bccb66fe76f71214f5abe08b1ac27ac8c
Svetlana Dyakova <dyakova030@gmail.com>
2026-06-22 16:13:54 +0300

    Добавить сериализаторы, вьюсеты и роутер для API

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,21 +1,57 @@
-from posts.models import Comment, Post
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
+from posts.models import Comment, Follow, Group, Post
+
+User = get_user_model()
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ("id", "title", "slug", "description")
+
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = SlugRelatedField(slug_field="username", read_only=True)
+    group = serializers.PrimaryKeyRelatedField(
+        queryset=Group.objects.all(), required=False
+    )
 
     class Meta:
-        fields = '__all__'
+        fields = "__all__"
         model = Post
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        read_only=True, slug_field="username"
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        fields = "__all__"
+        model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field="username",
+        read_only=True,
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field="username", queryset=User.objects.all()
     )
 
     class Meta:
-        fields = '__all__'
-        model = Comment
+        model = Follow
+        fields = ("user", "following")
+
+    def validate(self, data):
+        if self.context["request"].user == data["following"]:
+            raise serializers.ValidationError(
+                "Нельзя подписаться на самого себя"
+            )
+        return data
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,9 +1,20 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView,
 )
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register("posts", PostViewSet, basename="posts")
+router_v1.register("groups", GroupViewSet, basename="groups")
+router_v1.register(
+    r"posts/(?P<post_id>\d+)/comments", CommentViewSet, basename="comments"
+)
+router_v1.register("follow", FollowViewSet, basename="follow")
 
 urlpatterns = [
     path(
@@ -13,4 +24,5 @@ urlpatterns = [
     ),
     path("v1/jwt/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("v1/jwt/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path("v1/", include(router_v1.urls)),
 ]
